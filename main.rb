e7aa4cc0f25968c1fb0c018c871930e7e5239f0e
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-09-15 11:21:10 +0000

    Add Any

--- main.rb
+++ main.rb
@@ -1,32 +1,4 @@
-module Enumerable
-  def all?
-    list_items = self.instance_variable_get(:@list)
-    result = true
-    i=0
-    while i < list_items.length do
-      if yield(list_items[i])==false
-        result=false
-      end
-      i+=1
-    end
-      puts result
-  end
-
-  def filter
-    list_items = self.instance_variable_get(:@list)
-    result = []
-    i=0
-    while i < list_items.length do
-      if yield(list_items[i])==true
-        result << list_items[i]
-      end
-      i+=1
-    end
-    puts result
-  end
-
-end
-
+require 'enumerable.rb'
 class MyList 
   include Enumerable
   def initialize(list)
