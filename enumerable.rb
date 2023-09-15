0e802d119d3923ad3234a6202aaeb8c7672cad57
devendra-alt <devendramulewa171@gmail.com>
2023-09-15 17:11:39 +0530

    Fixed rubocop linter issues

--- enumerable.rb
+++ enumerable.rb
@@ -1,40 +1,34 @@
 module Enumerable
   def all?
-    list_items = self.instance_variable_get(:@list)
+    list_items = instance_variable_get(:@list)
     result = true
-    i=0
-    while i < list_items.length do
-      if yield(list_items[i])==false
-        result=false
-      end
-      i+=1
+    i = 0
+    while i < list_items.length
+      result = false if yield(list_items[i]) == false
+      i += 1
     end
-      puts result
+    puts result
   end
 
   def filter
-    list_items = self.instance_variable_get(:@list)
+    list_items = instance_variable_get(:@list)
     result = []
-    i=0
-    while i < list_items.length do
-      if yield(list_items[i])==true
-        result << list_items[i]
-      end
-      i+=1
+    i = 0
+    while i < list_items.length
+      result << list_items[i] if yield(list_items[i]) == true
+      i += 1
     end
-    puts result.to_s
+    puts result
   end
 
   def any?
-    list_items = self.instance_variable_get(:@list)
+    list_items = instance_variable_get(:@list)
     result = false
-    i=0
-    while i < list_items.length do
-      if yield(list_items[i])==true
-        result=true
-      end
-      i+=1
+    i = 0
+    while i < list_items.length
+      result = true if yield(list_items[i]) == true
+      i += 1
     end
-      puts result
+    puts result
   end
 end
--- main.rb
+++ main.rb
@@ -1,17 +1,16 @@
 require_relative 'enumerable'
 
-class MyList 
+class MyList
   include Enumerable
   def initialize(list)
-    @list=list
+    @list = list
   end
 end
 
 list = MyList.new([1, 2, 3, 4])
 
-list.all? {|e| e < 5}
-list.all? {|e| e > 5}
-list.any? {|e| e == 2}
-list.any? {|e| e == 5}
-list.filter {|e| e.even?}
-
+list.all? { |e| e < 5 }
+list.all? { |e| e > 5 }
+list.any? { |e| e == 2 }
+list.any? { |e| e == 5 }
+list.filter(&:even?)
