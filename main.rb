f407a17222921f90eecd593dd1905ae08a27d16d
devendra-alt <devendramulewa171@gmail.com>
2023-09-15 18:48:20 +0530

    Add each method on MyList

--- main.rb
+++ main.rb
@@ -2,15 +2,23 @@ require_relative 'enumerable'
 
 class MyList
   include Enumerable
-  def initialize(list)
+  def initialize(*list)
     @list = list
+  end
+
+  def each
+    for i in 0..@list.length do
+      yield(@list[i])
+      i+=1  
+    end
   end
 end
 
-list = MyList.new([1, 2, 3, 4])
+list = MyList.new(1, 2, 3, 4)
 
 list.all? { |e| e < 5 }
 list.all? { |e| e > 5 }
 list.any? { |e| e == 2 }
 list.any? { |e| e == 5 }
 list.filter(&:even?)
+list.each { |e| print "#{e} " }
