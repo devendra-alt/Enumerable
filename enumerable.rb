351da960ffe84e9b1572b14140248568b3429e51
devendra-alt <devendramulewa171@gmail.com>
2023-09-15 17:07:04 +0530

    formated enumerable.rb

--- enumerable.rb
+++ enumerable.rb
@@ -1,42 +1,40 @@
 module Enumerable
-    def all?
-      list_items = self.instance_variable_get(:@list)
-      result = true
-      i=0
-      while i < list_items.length do
-        if yield(list_items[i])==false
-          result=false
-        end
-        i+=1
+  def all?
+    list_items = self.instance_variable_get(:@list)
+    result = true
+    i=0
+    while i < list_items.length do
+      if yield(list_items[i])==false
+        result=false
       end
-        puts result
+      i+=1
     end
-  
-    def filter
-      list_items = self.instance_variable_get(:@list)
-      result = []
-      i=0
-      while i < list_items.length do
-        if yield(list_items[i])==true
-          result << list_items[i]
-        end
-        i+=1
-      end
       puts result
-    end
-  
-  
-    def any?
-      list_items = self.instance_variable_get(:@list)
-      result = false
-      i=0
-      while i < list_items.length do
-        if yield(list_items[i])==true
-          result=true
-        end
-        i+=1
-      end
-        puts result
-    end
-  
   end
+
+  def filter
+    list_items = self.instance_variable_get(:@list)
+    result = []
+    i=0
+    while i < list_items.length do
+      if yield(list_items[i])==true
+        result << list_items[i]
+      end
+      i+=1
+    end
+    puts result.to_s
+  end
+
+  def any?
+    list_items = self.instance_variable_get(:@list)
+    result = false
+    i=0
+    while i < list_items.length do
+      if yield(list_items[i])==true
+        result=true
+      end
+      i+=1
+    end
+      puts result
+  end
+end
